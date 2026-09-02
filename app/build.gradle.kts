857d25c5920e7ddcd7c45281bd6dc03ab438d30f
founder@edit.fund <founder>
2026-09-02 09:24:54 +0800

    添加新功能：XXX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    kotlin("android") version "1.9.10" apply false
-}
-
-plugins {
     id("com.android.application")
     kotlin("android")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		google()
+		mavenCentral()
+	}
+}
+
 rootProject.name = "demo-1-http-get"
 include(":app")
