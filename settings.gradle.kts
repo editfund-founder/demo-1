1839c6e960820926e362008d94488fec770774c9
founder@edit.fund <founder>
2026-09-02 09:46:43 +0800

    添加新功能：XXX

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,11 @@ pluginManagement {
 		google()
 		mavenCentral()
 	}
-}
-
-rootProject.name = "demo-1-http-get"
-include(":app")
-
-pluginManagement {
 	plugins {
 		id("com.android.application") version "8.2.2"
 		kotlin("android") version "1.9.10"
 	}
 }
+
+rootProject.name = "demo-1-http-get"
+include(":app")
